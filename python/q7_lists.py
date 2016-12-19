a8a00d894c474e605eb9c123b9252d7ce50d9531
pnayab <nayab.pallavi@gmail.com>
2016-12-19 14:01:29 -0500

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -110,4 +110,16 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
+    new_list = []
+    while len(list1) and len(list2):
+        if list1[0] < list2[0]:
+            new_list.append(list1.pop(0))
+        else:
+            new_list.append(list2.pop(0))
+    
+    new_list.extend(list1)
+    new_list.extend(list2)
+    return new_list
+   
+
     raise NotImplementedError

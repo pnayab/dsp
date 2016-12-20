e6c56bd5b216822348266f9b59db9d5ae0ff3284
pnayab <nayab.pallavi@gmail.com>
2016-12-20 15:51:29 -0500

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -36,3 +36,20 @@ degree_dict = {x:degree_list.count(x) for x in degree_list}
 print "There are %s different titles" % len(degree_dict)
 print "These are the frequencies %s" % degree_dict
 f.close()
+
+Q3:
+import csv
+
+f = open("faculty.csv")
+txt = f.read()[1:]
+header = txt.splitlines()[:1]
+lines = txt.splitlines()[1:]
+
+csv_f = csv.reader(lines)
+
+degree_list = []
+for row in csv_f:
+    degree_list.append(row[3])
+
+print degree_list
+f.close()

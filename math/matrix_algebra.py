8841c482a7a88c9792d5369c3e69fe802a739a46
pnayab <nayab.pallavi@gmail.com>
2016-12-24 16:00:05 -0500

    Update matrix_algebra.py

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -1,3 +1,22 @@
 # Matrix Algebra
-
-PLACE YOUR CODE HERE
+1.1:
+import numpy as np
+a = np.array([[1,2,3],[2,7,4]])
+print a.ndim
+=> 2
+1.2:
+import numpy as np
+a = np.array([[1,-1],[0,1]])
+print a.ndim
+=> 2
+1.3:
+import numpy as np
+a = np.array([[5,-1],[9,1],[6,0]])
+print a.ndim
+=> 2
+1.4:
+import numpy as np
+a = np.array([[3,-2,-1],[1,2,3]])
+print a.ndim
+=>2
+1.5:

21bbe1b88449fbe72fdf0bdeb1118974df944ccb
pnayab <nayab.pallavi@gmail.com>
2017-01-05 23:12:26 -0500

    Update advanced_python_dict.py

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -60,3 +60,36 @@ for i in range(length):
     faculty_dict[firstname,lastname] = faculty_list[i][1:]
 print faculty_dict
 f.close()
+
+Q8:
+  
+import csv
+
+f = open("faculty.csv")
+txt = f.read()[1:]
+header = txt.splitlines()[:1]
+lines = txt.splitlines()[1:]
+
+csv_f = csv.reader(lines)
+
+faculty_dict = {}
+faculty_list = []
+for row in csv_f:
+    faculty_list.append(row)
+
+length = len(faculty_list)
+for i in range(length):
+    if faculty_list[i][2].split()[0] == "Associate":
+        faculty_list[i][2] = "Associate Professor"
+    else:
+        faculty_list[i][2] = "Professor"
+    firstname = faculty_list[i][0].split()[0]
+    lastname = faculty_list[i][0].split()[-1]
+    new_name = (str(lastname) + " " + str(firstname))
+    faculty_dict[lastname,firstname] = faculty_list[i][1:]
+
+
+for key in sorted(faculty_dict):
+    print key, faculty_dict[key]
+    
+f.close()
